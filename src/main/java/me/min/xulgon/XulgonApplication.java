2371e99f66ea00ef48fd8d9a8e16920d1ab626ce
min <illogikally@gmail.com>
2021-05-24 19:58:41 +0700

    init

---
+++ src/main/java/me/min/xulgon/XulgonApplication.java
@@ -0,0 +1,13 @@
+package me.min.xulgon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XulgonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XulgonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/min/xulgon/XulgonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.min.xulgon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XulgonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
